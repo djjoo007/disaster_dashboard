30a2c2515b7f4d1bb303d5498c6a593f9a1b4b3e
KJ-Labs <36465042+KJ-Labs@users.noreply.github.com>
2020-11-10 19:24:08 -0800

    Updating so both front and back work

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ require('./routes/api-routes.js')(app);
 // Send every request to the React app
 // Define any API routes before this runs
 app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
 });
 
 app.listen(PORT, function() {
